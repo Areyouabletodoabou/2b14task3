8ade5a2e44268c466669efda4c2ff107c8ef9b68
Areyouabletodoabou <115679880+Areyouabletodoabou@users.noreply.github.com>
2022-10-13 11:01:36 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(0)
+})
